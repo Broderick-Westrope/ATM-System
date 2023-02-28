fadb496c71fee2252b599f82d3458092bd97d292
Broderick Westrope <broderickwestrope@gmail.com>
2023-03-01 09:54:49 +1100

    Add card number generation.

--- ATM System/AccountManager.cs
+++ ATM System/AccountManager.cs
@@ -1,9 +1,19 @@
 namespace ATM_System;
 
-public class AccountManagement
+public class AccountManager
 {
-    internal void CreateAccount()
+    internal void CreateAccount(string name, int pin)
     {
-        
+    }
+
+    internal void Login(int cardNumber, int pin)
+    {
+    }
+
+    int GenerateCardNumber()
+    {
+        Random rand = new Random();
+        int cardNum = rand.Next(100000, 999999);
+        return cardNum;
     }
 }
